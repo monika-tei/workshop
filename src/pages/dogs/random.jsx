6d08fab8398d9a02e0cb0867e02e3c4ad941792a
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-04-17 14:37:40 +0200

    okay it was all about /dogs/random

--- src/pages/dogs/random.jsx
+++ src/pages/dogs/random.jsx
@@ -1,17 +1,18 @@
-export default function RandomDog() {
+// DOM
+export default function RandomDog({ imgUrl }) {
   return (
     <>
-      <h1>Random Dog</h1>
-      <img src={imgUrl} alt="random dog"></img>
+      <h1>Random dog</h1>
+      <img src={imgUrl} alt="Random dog" />
     </>
   );
 }
 
+// BACKEND
 export async function getServerSideProps() {
   const api = "https://dog.ceo/api/breeds/image/random";
   const res = await fetch(api);
   const data = await res.json();
-  console.log(imgUrl);
 
   return {
     props: {
